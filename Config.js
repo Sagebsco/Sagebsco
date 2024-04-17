d224b9710176d1d30e8ef11fe0b347671755e39a
Sagebsco <167043882+Sagebsco@users.noreply.github.com>
2024-04-17 15:27:45 -0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,27 +2,27 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
+global.ownernumber = ['2347087675902']
+global.ownername = "💙𓆩‎sagebsco𓆪_✰"//owner name
 global.ytname = "YT: AyushBotz.inc"
-global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.socialm = "GitHub: sagebsco"
+global.location = "Nigerian"
 
-global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
+global.botname = 'sagebsco' //name of the bot
 
 //sticker details
-global.stickername = '💙𓆩‎✘AʏᴜSʜ✘𓆪_✰'
+global.stickername = '💙𓆩‎sagebsco𓆪_✰'
 global.packname = 'Sticker By'
-global.author = 'Maria Bot'
+global.author = 'sagebsco'
 //console view/theme
 global.themeemoji = '🧩'
 global.wm = "Ayush botz inc."
 
 //theme link
-global.link = 'https://whatsapp.com/channel/0029VaImo5ZG3R3qjKOdyr1I'
+global.link = 'https://whatsapp.com/channel/0029VaABx2I2ER6jTGKcSQ1v'
 
 //custom prefix
-global.prefa = ['.']
+global.prefa = ['$']
 
 //false=disable and true=enable
 global.welcome = false //auto welcome
